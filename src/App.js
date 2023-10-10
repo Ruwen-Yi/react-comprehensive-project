80f500c918ffb4fa78e31a985461ec1293fe5ae4
ruwen <ruwen.yi@hotmail.com>
2023-10-10 17:34:38 +0800

    feat(member-list):create static member list in form of buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
+import MemberList from "./MemberList";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen</h2>
+      {/* <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen</h2> */}
+      <MemberList />
     </div>
   );
 }
---
+++ src/MemberList.js
@@ -0,0 +1,8 @@
+export default function MemberList() {
+    return (
+        <div>
+            <button>Harry Potter</button>
+            <button>Peter Parker</button>
+        </div>
+    )
+}

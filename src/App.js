ba33ca381bdc163cac0eb5d0a401beb45f361163
ruwen <ruwen.yi@hotmail.com>
2023-10-11 14:18:44 +0800

    fix(module-import):fix TodoList module not found

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./styles.css";
 import MemberList from "./MemberList.js";
-import TodoList from "./TodoList";
+import TodoList from "./TodoList.js";
 
 export default function App() {
   return (
     <div>
-      {/* <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen</h2> */}
       <MemberList />
       <TodoList />
     </div>

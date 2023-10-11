098395dd7f91e352920ec6fdd86139e3e9c65773
ruwen <ruwen.yi@hotmail.com>
2023-10-11 14:26:25 +0800

    feat(todoList):add edit and cancel button

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,16 +3,22 @@ export default function TodoList() {
         <div>
             <ul>
                 <li>
-                    <label htmlFor="task-0">purchases a wand</label>
                     <input type="checkbox" id="task-0"/>
+                    <label htmlFor="task-0">purchases a wand</label>
+                    <button>Edit</button>
+                    <button>Cancel</button>
                 </li>
                 <li>
-                    <label htmlFor="task-1">buys school textbooks</label>
                     <input type="checkbox" id="task-1"/>
+                    <label htmlFor="task-1">buys school textbooks</label>
+                    <button>Edit</button>
+                    <button>Cancel</button>
                 </li>
                 <li>
-                    <label htmlFor="task-2">buys a cauldron</label>
                     <input type="checkbox" id="task-2"/>
+                    <label htmlFor="task-2">buys a cauldron</label>
+                    <button>Edit</button>
+                    <button>Cancel</button>
                 </li>
             </ul>
         </div>

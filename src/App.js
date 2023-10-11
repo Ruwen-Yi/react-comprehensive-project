3e0c79aa3b293c50b187dfcdc3d6aa3d50549cec
ruwen <ruwen.yi@hotmail.com>
2023-10-11 14:05:52 +0800

    style(App.js) delete the App component's template style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TodoList from "./TodoList";
 
 export default function App() {
   return (
-    <div className="App">
+    <div>
       {/* <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen</h2> */}
       <MemberList />

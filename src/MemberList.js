c83a71c13c4482ad76b1e3dab626bac3768f6bf2
ruwen <ruwen.yi@hotmail.com>
2023-10-13 20:46:27 +0800

    feat(MemberList-style):style the memberlist title board

--- src/MemberList.js
+++ src/MemberList.js
@@ -1,8 +1,59 @@
+import styled from 'styled-components';
+
+const StyledArrow = styled.div`
+    height: 100%;
+    padding: 0.5em 0;
+    font-size: 24px;
+    cursor: pointer;
+
+    &:hover {
+        font-weight: bold;
+        color: yellow;
+    }
+`
+
+const StyledMemberList = styled.div`
+    width: 40vw;
+    height: 10vh;
+    padding: 10px;
+    background-color: #E8C499;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const StyledMemberButton = styled.button`
+    width: auto;
+    height: 100%;
+    padding: 10px;
+    margin: 0 10px;
+    background: #deaa6b;
+    border: none;
+    outline: none;
+
+    color: white;
+    text-align: center;
+    font-size: 30px;
+    font-weight: bold;
+    
+    cursor: pointer;
+
+    &:hover{
+        color: yellow;
+    }
+`
+
 export default function MemberList() {
     return (
-        <div>
-            <button>Harry Potter</button>
-            <button>Peter Parker</button>
-        </div>
+        <StyledMemberList>
+            <StyledArrow>&#8592;</StyledArrow>
+            <div>
+                <StyledMemberButton>Harry Potter</StyledMemberButton>
+                <StyledMemberButton>Peter Parker</StyledMemberButton>
+                <StyledMemberButton>Jhon Smith</StyledMemberButton>
+            </div>
+            <StyledArrow>&#8594;</StyledArrow>
+        </StyledMemberList>
     )
 }

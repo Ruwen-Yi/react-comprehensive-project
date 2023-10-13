361a7f98b1276a61bda650fa74b21c63ae61df11
ruwen <ruwen.yi@hotmail.com>
2023-10-13 16:56:13 +0800

    feat(App-style):add style for the container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./css/global.css";
+import styles from "./css/App.module.css";      // implement scoped style by css-modules
 
 import MemberList from "./MemberList.js";
 import TodoList from "./TodoList.js";
 
 export default function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <MemberList />
       <TodoList />
     </div>

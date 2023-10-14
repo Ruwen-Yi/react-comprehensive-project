047dee47907eb7c00287b5f347ae0689d678ad19
ruwen <ruwen.yi@hotmail.com>
2023-10-14 15:23:40 +0800

    refactor(TodoList):break down the TodoList component

---
+++ src/components/TodoBoard/TodoItem.js
@@ -0,0 +1,10 @@
+export default function TodoItem() {
+    return (
+        <li>
+            <input type="checkbox" id="task-0" />
+            <label htmlFor="task-0">purchases a wand</label>
+            <button>Edit</button>
+            <button>Cancel</button>
+        </li>
+    )
+}
--- src/components/TodoBoard/TodoList.js
+++ src/components/TodoBoard/TodoList.js
@@ -1,24 +1,13 @@
+import TodoItem from "./TodoItem.js"
+
 export default function TodoList() {
     return (
         <ul>
-            <li>
-                <input type="checkbox" id="task-0" />
-                <label htmlFor="task-0">purchases a wand</label>
-                <button>Edit</button>
-                <button>Cancel</button>
-            </li>
-            <li>
-                <input type="checkbox" id="task-1" />
-                <label htmlFor="task-1">buys school textbooks</label>
-                <button>Edit</button>
-                <button>Cancel</button>
-            </li>
-            <li>
-                <input type="checkbox" id="task-2" />
-                <label htmlFor="task-2">buys a cauldron</label>
-                <button>Edit</button>
-                <button>Cancel</button>
-            </li>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
         </ul>
     )
 }

2f6007daf4080ffb0a20c2637105384bf45a8997
ruwen <ruwen.yi@hotmail.com>
2023-10-16 15:22:02 +0800

    feat(style):styling the todolist board

--- src/components/TodoBoard/TodoList.js
+++ src/components/TodoBoard/TodoList.js
@@ -5,34 +5,43 @@ import styled from "styled-components"
 const StyledCListGroupItem = styled(CListGroupItem)`
     width:35vw;
     
-    input[type='checkbox'],
-    input[type='checkbox']:checked {    
-        border-color: #a4a9ac;
-        box-shadow: none;
+    input[type='checkbox'] {
+        border: 3px solid #212631;
+        background-color: transparent;
     }
-
     input[type='checkbox']:checked {
+        border-color: #a4a9ac;
         background-color: #a4a9ac;
     }
-
     input[type='checkbox']:checked + label {
         font-style: italic;
         color: #a4a9ac;
         text-decoration: line-through;
     }
+
+    & {
+        border: 0;
+        background-color: transparent;
+        font-size: 1.5rem;
+
+        display: flex;
+        justify-content: space-between;      
+    }
+    &:hover {
+        border: 3px solid #212631;
+        border-radius: 0;
+    }
 `
 
-// use coreUI's built-in classes to layout three containers element.
-// - containers of a list item, a input/label group, and a button group.
-// - a list item is composed of a input/label group(i.e, CFormCheck), and a button group.
-let listItemContainerClasses = "d-flex justify-content-between fs-5 bg-transparent";
+// use coreUI's built-in classes to layout containers
+// of the input/label group and the button group.
 let labelContainerClasses = "d-inline-block text-wrap text-break";
 let buttonContainerClasses = "d-inline-flex align-items-end z-1";
 
 export default function TodoList() {
     return (
         <CListGroup>
-            <StyledCListGroupItem className={listItemContainerClasses}>
+            <StyledCListGroupItem>
                 <div className={labelContainerClasses}>
                 {/* an id is necessary for CFormCheck to be clickable */}
                 <CFormCheck 
@@ -47,7 +56,7 @@ export default function TodoList() {
                     <CButton className="rounded-pill" color="dark">Cancel</CButton>
                 </div>
             </StyledCListGroupItem>
-            <StyledCListGroupItem className={listItemContainerClasses}>
+            <StyledCListGroupItem>
                 <div className={labelContainerClasses}>
                 {/* an id is necessary for CFormCheck to be clickable */}
                 <CFormCheck 
@@ -56,10 +65,6 @@ export default function TodoList() {
                     label="purchases a wand" 
                     id='input-label-1'
                 />
-                </div>
-                <div className={buttonContainerClasses}>
-                    <CButton className="me-1 rounded-pill" color="dark">Edit</CButton>
-                    <CButton className="rounded-pill" color="dark">Cancel</CButton>
                 </div>
             </StyledCListGroupItem>
         </CListGroup>

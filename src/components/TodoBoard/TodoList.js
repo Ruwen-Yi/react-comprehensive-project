4d17f03bbd7b7a881026bff36283a5a5ac110309
ruwen <ruwen.yi@hotmail.com>
2023-10-20 15:11:02 +0800

    fix(TodoList): declare const variable before using it

--- src/components/TodoBoard/TodoList.js
+++ src/components/TodoBoard/TodoList.js
@@ -18,13 +18,21 @@ export default function TodoList({ todoList, selectedMember, onTodoChange }) {
 function TodoItem({ todo, onTodoChange}) {
     const { id, text, state} = todo;
 
+    // a todo item is checked if its state is 'done'
+    const isChecked = state === 'done' ? true : false;
+
     // react state - control whether the todo item is being editing
     const [isEditing, setIsEditing] = useState(false);
 
+    // the button is a 'Save' button when a todo is being editing
+    // otherwise it is a 'Edit' button 
     const EditOrSaveBtn = isEditing ? 'Save' : 'Edit';
+
+    const onSave = () => setIsEditing(false);
+    const onEdit = () => setIsEditing(true);
     const EditOrSaveHandler = isEditing ? onSave : onEdit;
 
-    // the todo content is an text input when being editing
+    // the todo content is an text input when a todo being editing
     // otherwise it is an uneditable label
     const todoContent = isEditing ? 
         (<input 
@@ -38,7 +46,7 @@ function TodoItem({ todo, onTodoChange}) {
         >
             {text}
         </label>)
-        
+
     /**
      * this event listener fires when the text or state of a todo item changed
      * @param {Event} e 
@@ -56,15 +64,10 @@ function TodoItem({ todo, onTodoChange}) {
         onTodoChange(newTodo);      // update the todo list with the new todo item
     }
 
-    const onSave = () => setIsEditing(false);
-    const onEdit = () => setIsEditing(true);
     
     function onDelete(e) {
         
     }
-
-    // a todo item is checked if its state is 'done'
-    const isChecked = state === 'done' ? true : false;
     
     return (
         <div className='custom-list-group-item-container'>

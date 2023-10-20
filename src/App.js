e4f5b6120827c8198fc5f7c4e8b71015d6c10795
ruwen <ruwen.yi@hotmail.com>
2023-10-20 14:35:16 +0800

    feat(TodoList): control the mutation of todo text and todo state by react state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from './css/App.module.css';      // implement scoped style by css-modules
 
@@ -6,13 +6,19 @@ import MemberBoard from './components/MemberBoard/MemberBoard.js';
 import TodoBoard from './components/TodoBoard/TodoBoard.js';
 import data from './data/data.js';
 
-
 export default function App() {
     // state: the id of the selected member
     const [selectedMember, setSelectedMember] = useState(0);
 
-    // the todo list of the selected member
-    const todoList = data.find((member) => member.id === selectedMember).list;
+    // state: the todo list of the selected member
+    const [todoList, setTodoList] = useState(data.find((member) => member.id === selectedMember).list);
+
+    // save the latest todo list before switching to a new member
+    useEffect(() => {
+        return () => {
+            data.find((member) => member.id === selectedMember).list = todoList;
+        }
+    },[todoList]);
     
     // the list of all members with their id and name
     const memberList = data.map(member => {
@@ -20,7 +26,7 @@ export default function App() {
             id: member.id,
             name: `${member.fstName} ${member.lstName}`
         }
-    })
+    });
 
     /**
      * set the selectedMember state to the id of the selected Member
@@ -29,6 +35,16 @@ export default function App() {
     function handleMemberChange(e) {
         const nextId = +e.target.value;     // convert a string to a number
         setSelectedMember(nextId);
+        setTodoList(data.find((member) => member.id === nextId).list);
+    }
+
+    function onTodoChange(newTodo) {
+        // console.log(newTodo);
+        const newTodoId = newTodo.id;
+        const newTodoList = todoList.map((todo) => (
+            todo.id === newTodoId ? newTodo : todo
+        ));
+        setTodoList(newTodoList);
     }
 
     return (
@@ -39,8 +55,10 @@ export default function App() {
                 onMemberChange={handleMemberChange}
             />
             <TodoBoard 
+                key={selectedMember}
                 todoList={todoList}
                 selectedMember={selectedMember}
+                onTodoChange={onTodoChange}
             />
         </div>
     );
--- src/components/TodoBoard/TodoList.js
+++ src/components/TodoBoard/TodoList.js
@@ -1,26 +1,59 @@
 import { useState } from 'react';
 
-export default function TodoList({ todoList, selectedMember }) {
+export default function TodoList({ todoList, selectedMember, onTodoChange }) {
 
     return (
         <div className="list-group">
             {todoList.map((todo) => (
                 <TodoItem
                     key={`${selectedMember}-${todo.id}`}
-                    {...todo}
+                    todo={todo}
+                    onTodoChange={onTodoChange}
                 />
             ))}
         </div>
     )
 }
 
-function TodoItem({ id, text, state}) {
+function TodoItem({ todo, onTodoChange}) {
+    const { id, text, state} = todo;
+
     const [isEditing, setIsEditing] = useState(false);
 
     const EditOrSaveBtn = isEditing ? 'Save' : 'Edit';
-    
+    const EditOrSaveHandler = isEditing ? onSave : onEdit;
+    const todoContent = isEditing ? 
+        (<input 
+            className="list-group-item"
+            value={text}
+            onChange={onChange}
+        />) : 
+        (<label 
+            className="list-group-item"
+            htmlFor={id}
+        >
+            {text}
+        </label>)
+        
+    function onChange(e) {
+        let newTodo = {...todo};
+
+        if (e.target.type === 'text') {
+            newTodo.text = e.target.value;
+        }
+        else if (e.target.type === 'checkbox') {
+            newTodo.state = e.target.checked ? 'done' : 'undo';
+        }
+        
+        onTodoChange(newTodo);
+    }
+
+    function onSave(e) {
+        setIsEditing(false);
+    }
+
     function onEdit(e) {
-        setIsEditing(!isEditing);
+        setIsEditing(true);
     }
     
     function onDelete(e) {
@@ -37,18 +70,21 @@ function TodoItem({ id, text, state}) {
                     type="checkbox" 
                     defaultChecked={isChecked}
                     id={id}
+                    onChange={onChange}
                 />
+                {todoContent}
+                {/* 
                 <label 
                     className="list-group-item"
                     htmlFor={id}
                 >
                     {text}
-                </label>
+                </label> */}
             </div>
             <div className='custom-buttons-wrapper'>
                 <button 
                     className="btn btn-dark rounded-pill me-1"
-                    onClick={onEdit}    
+                    onClick={EditOrSaveHandler}    
                 >
                     {EditOrSaveBtn}
                 </button>

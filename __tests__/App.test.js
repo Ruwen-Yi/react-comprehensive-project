f738e788042492547151c91d258af76d30be8a62
ruwen <ruwen.yi@hotmail.com>
2023-10-24 15:09:13 +0800

    test(App.js):adjust existing test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -8,6 +8,8 @@ import userEvent from '@testing-library/user-event';
 import App from '../src/App.js';
 
 describe('App component', () => {
+    test('should toggle member if ')
+
     test('should delete a todo item if its delete button was clicked', async () => { 
         const user = userEvent.setup();
 
@@ -15,10 +17,14 @@ describe('App component', () => {
 
         const deleteBtnElement = screen.getAllByText('Delete')[0];
 
+        // before deletion, the todo item should be on the screen
+        const todoItemElement = screen.queryByText('todo 0');
+        expect(todoItemElement).toBeInTheDocument();
+
         // clicking the delete button, the todo item should be removed 
         await user.click(deleteBtnElement);
 
-        const deletedTodoItemElement = screen.queryByText('purchases a wand');
+        const deletedTodoItemElement = screen.queryByText('todo 0');
         expect(deletedTodoItemElement).toBeNull();
     });
 })

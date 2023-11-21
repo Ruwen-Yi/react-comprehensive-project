3d48ac5d9c3b7a21123c87dea8a9d8c3a02aa54f
ruwen <ruwen.yi@hotmail.com>
2023-11-21 18:54:44 +0800

    feat(client): establish a simple client side

--- chatRoom/client/src/Chatroom.js
+++ chatRoom/client/src/Chatroom.js
@@ -1,12 +1,37 @@
+import { useRef, useEffect } from 'react';
+
 export default function Chatroom() {
+    const wsRef = useRef();
+    
+    // connect to websocket server on mount
+    useEffect(() => {
+        const client = new WebSocket('ws://localhost:8080');
+        client.onerror = () => console.error;
+        client.onopen = () => {
+            console.log('Connected');
+        };
+        client.onmessage = (data) => {
+            console.log('received: %s', data);
+        };
+
+        wsRef.current = client;
+
+        return () => wsRef.current.close()
+    }, []);
+    
+    const sendMessage = (e) => {
+        const message = document.getElementById('user-input');
+        wsRef.current.send(message.value);
+    }
+
     return (
         <>
             <div>
                 view
             </div>
             <div>
-                <input type="text" />
-                <button>submit</button>
+                <input type="text" id="user-input"/>
+                <button onClick={sendMessage}>submit</button>
             </div>
         </>
     )

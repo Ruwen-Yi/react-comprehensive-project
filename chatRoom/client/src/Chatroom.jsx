e534713ab15de13f2cfab919220b1118f4f922f6
Ruwen-Yi <ruwen.yi@hotmail.com>
2023-11-25 18:37:18 +0800

    feat(client): enable client send message to server

--- chatRoom/client/src/Chatroom.jsx
+++ chatRoom/client/src/Chatroom.jsx
@@ -1,36 +1,49 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 export default function Chatroom() {
+    const [text, setText] = useState(""); 
     const wsRef = useRef();
     
     // connect to websocket server on mount
     useEffect(() => {
         const client = new WebSocket('ws://localhost:8080');
-        client.onerror = () => console.error;
+        client.onerror = console.error;
         client.onopen = () => {
             console.log('Connected');
         };
-        client.onmessage = (data) => {
-            console.log('received: %s', data);
+        client.onmessage = (e) => {
+            console.log('received: %s', e.data);
         };
 
         wsRef.current = client;
 
-        return () => wsRef.current.close()
+        return () => {
+            wsRef.current.close();
+            console.log("Connection clean up")
+        }
     }, []);
     
     const sendMessage = (e) => {
-        const message = document.getElementById('user-input');
-        wsRef.current.send(message.value);
+        wsRef.current.send(text);
+        setText("");
+    }
+
+    const updateUserInput = (e) => {
+        setText(e.target.value);
     }
 
     return (
         <>
             <div>
-                chat view
+                view
             </div>
             <div>
-                <input type="text" id="user-input"/>
+                <input 
+                    type="text" 
+                    id="user-input"
+                    value={text}
+                    onChange={updateUserInput}
+                />
                 <button onClick={sendMessage}>submit</button>
             </div>
         </>

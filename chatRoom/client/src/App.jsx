2d0a62625baf928406f94a63a2f34e1fdf6d7129
ruwen <ruwen.yi@hotmail.com>
2023-11-29 18:54:00 +0800

    feat(css): style the container of the chat app

--- chatRoom/client/src/App.jsx
+++ chatRoom/client/src/App.jsx
@@ -1,7 +1,15 @@
 import Chatroom from "./Chatroom.jsx";
 function App() {
   return (
-    <div>
+    <div className="
+    flex
+    flex-col
+    h-[80vh] 
+    w-[60vw]
+    bg-white 
+    rounded-lg
+    shadow-lg
+    ">
         <Chatroom />
     </div>
   );

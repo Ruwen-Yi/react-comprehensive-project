49c0a5cf7b6ba74ad8f57cbc6bb5bbcfc673af1d
ruwen <ruwen.yi@hotmail.com>
2023-12-06 12:14:15 +0800

    refactor(chatroom style): change chatroom's border-radius to vh unit

--- chatRoom/client/src/App.jsx
+++ chatRoom/client/src/App.jsx
@@ -2,7 +2,7 @@ import Chatroom from "./Chatroom.jsx";
 function App() {
     return (
         <div
-            className="flex flex-col h-[80vh] w-[60vw] rounded-xl shadow-2xl overflow-hidden"
+            className="flex flex-col h-[80vh] w-[60vw] rounded-[3vh] shadow-2xl overflow-hidden"
         >
             <Chatroom />
         </div>

778d32766a31842d9afa1ae33857e6fae84aa7c8
Ruwen-Yi <ruwen.yi@hotmail.com>
2024-03-27 18:33:01 +1030

    new update

--- todolist/jest.config.js
+++ todolist/jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '<rootDir>/__mocks__/fileMock.js',
-        '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-        'data\.(js)$': '<rootDir>/__mocks__/dataMock.js'
+            '<rootDir>/src/__mocks__/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.js',
+        'data\.(js)$': '<rootDir>/src/__mocks__/dataMock.js'
     },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{js,jsx}',
+        '!**/node_modules/**',
+        '!**/vendor/**',
+    ],
 };
---
+++ todolist/src/__tests__/App.test.js
@@ -0,0 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event';
+
+import App from '../App.js';
+
+describe('App component', () => {
+    test('should toggle member if ')
+
+    test('should delete a todo item if its delete button was clicked', async () => { 
+        const user = userEvent.setup();
+
+        render(<App />)
+
+        const deleteBtnElement = screen.getAllByText('Delete')[0];
+
+        // before deletion, the todo item should be on the screen
+        const todoItemElement = screen.queryByText('todo 0');
+        expect(todoItemElement).toBeInTheDocument();
+
+        // clicking the delete button, the todo item should be removed 
+        await user.click(deleteBtnElement);
+
+        const deletedTodoItemElement = screen.queryByText('todo 0');
+        expect(deletedTodoItemElement).toBeNull();
+    });
+})
